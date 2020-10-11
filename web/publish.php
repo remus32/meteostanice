d75817d599e8da165899d0fa527e452f0842c494
Matyáš Bobek <matyas.bobek@gmail.com>
2020-10-11 19:20:25 +0200

    Web v2

--- web/publish.php
+++ web/publish.php
@@ -1,34 +1,24 @@
 <?php
 require_once "common.inc";
 
-/**
- * Spočítá průměr fieldů v poli objektů.
- */
-function avgField($arr, $fieldName) {
-  if (count($arr) === 0) return null;
-
-  $sum = 0;
-  foreach ($arr as $el) {
-    $sum += $el[$fieldName];
-  }
-  return $sum / count($arr);
-}
-
 if ($_GET['key'] !== $_ENV['API_KEY']) die();
 
 // https://stackoverflow.com/questions/7047870/issue-reading-http-request-body-from-a-json-post-in-php
 $input_json = file_get_contents('php://input');
 $input = json_decode($input_json, true);
 
-$stmt = $dbh->prepare('INSERT INTO measurements("time", "temp", "hum", "pres") VALUES(?, ?, ?, ?)');
-$stmt->execute(array(
-  unix2wstime(time()) - 1, // Měření je za předchozí interval
+$stmt = $dbh->prepare('INSERT INTO measurements("period", "temp", "hum", "pres") VALUES(?, ?, ?, ?)');
 
-  // Převod z fixed-point integerů do floatů
-  avgField($input['bme'], 'temp') / 100,
-  avgField($input['bme'], 'hum') / 1024,
-  avgField($input['bme'], 'pres') / 256
-));
+foreach ($input as $id => $measurement) {
+ $stmt->execute(array(
+    unix2wstime(time()) - ($id + 1), // Měření je za předchozí interval
+
+    // Převod z fixed-point integerů do floatů
+    $measurement['tsum'] / $measurement['n_bme'] / 100,
+    $measurement['hsum'] / $measurement['n_bme'] / 1024,
+    $measurement['psum'] / $measurement['n_bme'] / 256
+  ));
+}
 
 // Kolik sekund už uběhlo od začátku aktuálního intervalu
 $elapsed_period_time = time() - wstime2unix(unix2wstime(time()));
